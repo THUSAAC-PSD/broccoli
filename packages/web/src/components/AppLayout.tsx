14a0f14ae4ffac131d95b5516c81b6efc05572d5
peop1e <Cqq060213@163.com>
2025-10-20 00:19:44 +0800

    feat: basic navbar

--- packages/web/src/components/AppLayout.tsx
+++ packages/web/src/components/AppLayout.tsx
@@ -1,6 +1,5 @@
 import {
   SidebarProvider,
-  SidebarTrigger,
   SidebarInset,
 } from '@/components/ui/sidebar';
 import { Sidebar } from './Sidebar';
@@ -9,19 +8,18 @@ interface AppLayoutProps {
   children: React.ReactNode;
 }
 
+import { Navbar } from '@/components/Navbar';
+
 export function AppLayout({ children }: AppLayoutProps) {
   return (
-    <SidebarProvider>
-      <Sidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-          <div className="flex items-center gap-2 text-sm">
-            <span className="font-semibold">Online Judge Platform</span>
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col">{children}</div>
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <SidebarProvider>
+        <Sidebar />
+        <SidebarInset>
+          <Navbar />
+          <div className="flex flex-1 flex-col">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
   );
 }
