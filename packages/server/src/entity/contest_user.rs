55b6a203f9d24a1fe34313e31efd3dd0ea276498
HeziCyan <hezicyan@gmail.com>
2026-01-26 19:11:18 +0800

    feat(server): implement database initialization and define database entities

---
+++ packages/server/src/entity/contest_user.rs
@@ -0,0 +1,18 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[sea_orm::model]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "contest_user")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub contest_id: i32,
+    #[sea_orm(primary_key)]
+    pub user_id: i32,
+    #[sea_orm(belongs_to, from = "contest_id", to = "id")]
+    pub contest: Option<super::contest::Entity>,
+    #[sea_orm(belongs_to, from = "user_id", to = "id")]
+    pub user: Option<super::user::Entity>,
+}
+
+impl ActiveModelBehavior for ActiveModel {}
--- packages/server/src/state.rs
+++ packages/server/src/state.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 
 use plugin_core::traits::PluginManager;
+use sea_orm::DatabaseConnection;
 
 #[derive(Clone)]
 pub struct AppState {
     pub plugins: Arc<dyn PluginManager>,
+    pub db: DatabaseConnection,
 }
