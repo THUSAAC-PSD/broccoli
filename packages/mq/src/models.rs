8056f801c3084b4d33a6b0999c3ea57deba82539
zesty_fox <zhs24@mails.tsinghua.edu.cn>
2026-02-02 00:13:55 +0800

    feat(mq): add mq package

---
+++ packages/mq/src/models.rs
@@ -0,0 +1,23 @@
+use broccoli_queue::queue::BroccoliQueueBuilder;
+pub use broccoli_queue::{
+    brokers::broker::BrokerMessage, error::BroccoliError, queue::BroccoliQueue,
+};
+
+use crate::error::MqError;
+
+// TODO: replace with our own Mq trait
+pub type MqQueue = BroccoliQueue;
+pub type MqBuilder = BroccoliQueueBuilder;
+
+pub struct MqConfig {
+    pub url: String,
+    pub pool_size: u8,
+}
+
+pub async fn init_mq(config: MqConfig) -> Result<MqQueue, MqError> {
+    BroccoliQueue::builder(&config.url)
+        .pool_connections(config.pool_size)
+        .build()
+        .await
+        .map_err(MqError::from)
+}
