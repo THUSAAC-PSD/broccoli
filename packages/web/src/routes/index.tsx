06e15eb91d439a463970469525dd9abbe316b742
HeziCyan <hezicyan@gmail.com>
2026-02-09 16:05:16 +0800

    Revert "feat(web): implement tanstack router with file-based routing"

--- packages/web/src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import { ProblemPage } from '@/pages/ProblemPage';
-
-export const Route = createFileRoute('/')({
-  component: ProblemPage,
-});
