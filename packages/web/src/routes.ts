46c61f53f017d61e6678dbf9e3b0f3e7b4c931a0
HeziCyan <hezicyan@gmail.com>
2026-02-09 16:20:20 +0800

    feat(web): implement react router with file-based routing
    Migrate from tanstack router to react router, for future needs of using
    both file- and code-based routing.

---
+++ packages/web/src/routes.ts
@@ -0,0 +1,8 @@
+import type { RouteConfig } from '@react-router/dev/routes';
+import { flatRoutes } from '@react-router/fs-routes';
+
+export default [
+  // TODO: Plugin routes
+
+  ...(await flatRoutes()),
+] satisfies RouteConfig;
---
+++ packages/web/src/routes/_index.tsx
@@ -0,0 +1,6 @@
+import { ProblemPage } from '@/pages/ProblemPage';
+
+export default function Overview() {
+  // TODO: replace this with an overview page
+  return <ProblemPage />;
+}
